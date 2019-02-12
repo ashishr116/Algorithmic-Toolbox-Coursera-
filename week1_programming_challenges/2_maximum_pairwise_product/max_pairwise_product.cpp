b1c1b7e34060af0153aeda30100a77b130803325
ashishr116 <32428296+ashishr116@users.noreply.github.com>
2019-02-13 00:34:42 +0530

    Add files via upload

---
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,element,m1=0,m2=0;
+    long long int pdt;
+    cin>>n;
+    if(n==1)
+    {   cin>>element;
+        m1=element;
+        m2=1;
+    }
+    else{
+    for(int i=0;i<n;i++)
+    {
+        cin>>element;
+        if(element>m1)
+        {
+            m2=m1;
+            m1=element;
+        }
+        else if(element>m2)
+        {
+            m2=element;
+        }
+    }
+    }
+    pdt=(long long int)m1*m2;
+    cout<<pdt;
+}
