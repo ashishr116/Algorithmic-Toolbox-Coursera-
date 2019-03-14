ded4d2a91ac8368cce58854ef9a9c4adb143ae8b
Ashish Gupta <ashishr116@gmail.com>
2019-03-14 17:37:00 +0530

    Add files via upload

---
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int get_change(int m) {
+    int steps=0;
+    if(m<5)
+    {
+        return m;
+    }
+  while(m>=5)
+  {
+    if(m>=10)
+    {
+        steps=steps+m/10;
+        m=m%10;
+    }
+    else if(m>=5)
+    {
+        steps=steps+m/5;
+        m=m%5;
+    }
+  }
+  steps=steps+m;
+  return steps;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
---
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::max;
+
+int compute_min_refills(int dist, int tank, vector<int> & stops) {
+
+for(int i=1;i<stops.size();i++)
+{
+    if(stops[i]-stops[i-1]>tank)
+    {
+        return -1;
+    }
+}
+if(stops[0]>tank)
+{
+    return -1;
+}
+if((dist-stops[stops.size()-1])>tank)
+{
+    return -1;
+}
+int coun=0,i=0,x=0,d1=tank;
+while(i<stops.size())
+{
+    x=stops[i];
+    if(x>d1)
+    {
+        d1=stops[i-1]+tank;
+        coun++;
+    }
+    i++;
+}
+if(d1<dist)
+{
+    coun++;
+}
+return coun;
+}
+
+
+int main() {
+    int d = 0;
+    cin >> d;
+    int m = 0;
+    cin >> m;
+    int n = 0;
+    cin >> n;
+
+    vector<int> stops(n);
+    for (size_t i = 0; i < n; ++i)
+        cin >> stops.at(i);
+
+    cout << compute_min_refills(d, m, stops) << "\n";
+
+    return 0;
+}
